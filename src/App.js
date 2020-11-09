496556bb45a26f773a1e11c51ad791a85c8621cd
Clare Jackson <clare-jackson@lambdastudents.com>
2020-11-08 19:55:21 -0500

    Pizza.js form complete. Schema and cypress next.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,128 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { Route, Link, Switch, Redirect } from 'react-router-dom';
+import Pizza from './Pizza';
+import  * as yup from 'yup';
+import schema from './formSchema'
+import Home from './Home'
+
+const initialFormValues = {
+  name: '',
+  //Dropdown
+  size: '',
+  //Checkboxes
+  toppings: {
+    pepperoni: false,
+    sausage: false,
+    onion: false,
+    olives: false,
+    peppers: false
+  },
+  instructions: ''
+}
+const initialFormErrors = {
+  name: '',
+  size: ''
+}
+const initialOrders = []
+const initialDisabled = true
+
 
 const App = () => {
+  const [orders, setOrders] = useState(initialOrders)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const postNewOrder = newOrder => {
+    axios.post('http://reqres.in/api/users', newOrder)
+      .then(response => {
+        setOrders([...orders, response.data])
+      })
+      .catch(error => {
+        debugger
+        console.log(error)
+      })
+      .finally(() => {
+        setFormValues(initialFormValues)
+      })
+  }
+
+  const validate = (name, value) => {
+    //for yup schema
+  }
+
+  const inputChange = (name, value) => {
+    validate(name, value)
+    setFormValues({
+      ...formValues, 
+      [name]: value
+    })
+  }
+
+  const formSubmit = () => {
+    const newOrder = {
+      name: formValues.name.trim(),
+      size: formValues.size,
+      toppings: formValues.toppings,
+      instructions: formValues.instructions.trim()
+    }
+    postNewOrder(newOrder)
+  }
+
+  useEffect (() => {
+    console.log(formValues)
+  }, [formValues])
+
+  useEffect (() => {
+    schema.isValid(formValues)
+      .then(valid => {
+        setDisabled(!valid)
+      })
+  }, [formValues])
+
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <nav>
+        <h1>Lambda Eats</h1>
+        <div className="navButtons">
+          <Link to='/pizza' id='orderForm'>Pizza Order</Link>
+          <Link to='/help' id='help'>Help</Link>
+          <Link to='/' id='home'>Home</Link>
+        </div>
+      </nav>
+      
+      <Switch>
+        <Route path='/help'>
+          <h1>Hungry? We are here to help!</h1>
+        </Route>
+
+        <Route path='/pizza'>
+          <Pizza 
+          values={formValues}
+          change={inputChange}
+          submit={formSubmit}
+          disabled={disabled}
+          errors={formErrors}
+          />
+          {
+            orders.map((order, index) => {
+              return <div key={index}>
+                      <h2>{order.name}</h2>
+                      <p>{order.size}</p>
+                      <p>{order.toppings}</p>
+                      <p>{order.instructions}</p>
+                      </div>
+            })
+          }
+        </Route>
+
+        <Route exact path='/'>
+          <Home />
+        </Route>
+
+        <Redirect to='/' />
+      </Switch>
     </>
   );
 };
---
+++ src/Pizza.js
@@ -0,0 +1,116 @@
+import React from 'react';
+
+export default function Pizza(props) {
+  const {
+    values,
+    submit,
+    change,
+    disabled,
+    errors,
+  } = props
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+    submit()
+  }
+
+  const onChange = evt => {
+    const { name, value, type, checked } = evt.target
+    const valueToUse = type === 'checkbox' ? checked : value
+    change(name, valueToUse)
+  }
+
+ return (
+   <form onSubmit={onSubmit}>
+     <h1>Order Here!</h1>
+
+     <button disabled={disabled}>Add to Order</button>
+
+     <div className='errors'>
+          <div>{errors.name}</div>
+          <div>{errors.size}</div>
+     </div>
+
+     <div>
+        <h2>Order Form</h2>
+
+        <label>Name:    </label>
+        <input 
+        value={values.name}
+        onChange={onChange}
+        name='name'
+        type='text'
+        />
+
+        <label>Pizza Size:       </label>
+        <select 
+        onChange={onChange}
+        value={values.size}
+        name='size'
+        >
+           <option value=''>- Select an option -</option>
+            <option value='8inch'>8 inch</option>
+            <option value='12inch'>12 inch</option>
+            <option value='16inch'>16 inch</option>
+        </select>
+
+        <h3>Toppings:     </h3>
+        <label>Pepperoni     </label>
+        <input
+        type='radio'
+        name='toppings'
+        value='pepperoni'
+        checked={values.toppings === 'pepperoni'}
+        onChange={onChange}
+        />
+        <label>Sausage    </label>
+        <input
+        type='radio'
+        name='toppings'
+        value='sausage'
+        checked={values.toppings === 'sausage'}
+        onChange={onChange}
+        />
+        <label>Onions     </label>
+        <input
+        type='radio'
+        name='toppings'
+        value='onions'
+        checked={values.toppings === 'onions'}
+        onChange={onChange}
+        />
+        <label>Olives    </label>
+        <input
+        type='radio'
+        name='toppings'
+        value='olives'
+        checked={values.toppings === 'olives'}
+        onChange={onChange}
+        />
+        <label>Pepppers     </label>
+        <input
+        type='radio'
+        name='toppings'
+        value='peppers'
+        checked={values.toppings === 'peppers'}
+        onChange={onChange}
+        />
+
+        <h3>Any more info for us?</h3>
+        <label>Special Instructions:    </label>
+        <input 
+        value={values.instructions}
+        onChange={onChange}
+        name='instructions'
+        type='text'
+        />
+
+
+
+
+
+     </div>
+
+   </form>
+ )
+}
